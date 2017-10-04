e19b4c1c2c70354d4aaddf9d3e9d1575935eb626
ntu_ror_ubuntu <rubyyang0615@gmail.com>
2017-10-04 17:49:41 +0800

    form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { MatDialog, MatDialogRef, MatHint } from '@angular/material';
 
 @Component({
   selector: 'app-login',
